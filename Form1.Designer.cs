37c536db7ba7af3d988130be57e83d9d433e4dde
carbon4dev <39811359+carbon4dev@users.noreply.github.com>
2018-06-04 17:21:34 -0300

    ADD ALL FILES

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Client
+{
+    partial class frmClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmClient));
+            this.lstClients = new System.Windows.Forms.ListBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.txtMsgs = new MetroFramework.Controls.MetroTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtUserName = new MetroFramework.Controls.MetroTextBox();
+            this.btnLogin = new MetroFramework.Controls.MetroButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSend = new MetroFramework.Controls.MetroButton();
+            this.txtSend = new MetroFramework.Controls.MetroTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstClients
+            // 
+            this.lstClients.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstClients.BackColor = System.Drawing.Color.Black;
+            this.lstClients.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstClients.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstClients.ForeColor = System.Drawing.Color.White;
+            this.lstClients.FormattingEnabled = true;
+            this.lstClients.ItemHeight = 20;
+            this.lstClients.Location = new System.Drawing.Point(3, 3);
+            this.lstClients.Name = "lstClients";
+            this.lstClients.Size = new System.Drawing.Size(230, 280);
+            this.lstClients.TabIndex = 0;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(3, 1);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(48, 19);
+            this.metroLabel1.TabIndex = 7;
+            this.metroLabel1.Text = "Name:";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // txtMsgs
+            // 
+            this.txtMsgs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.txtMsgs.CustomButton.Image = null;
+            this.txtMsgs.CustomButton.Location = new System.Drawing.Point(222, 2);
+            this.txtMsgs.CustomButton.Name = "";
+            this.txtMsgs.CustomButton.Size = new System.Drawing.Size(291, 291);
+            this.txtMsgs.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtMsgs.CustomButton.TabIndex = 1;
+            this.txtMsgs.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtMsgs.CustomButton.UseSelectable = true;
+            this.txtMsgs.CustomButton.Visible = false;
+            this.txtMsgs.Lines = new string[0];
+            this.txtMsgs.Location = new System.Drawing.Point(265, 63);
+            this.txtMsgs.MaxLength = 32767;
+            this.txtMsgs.Multiline = true;
+            this.txtMsgs.Name = "txtMsgs";
+            this.txtMsgs.PasswordChar = '\0';
+            this.txtMsgs.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtMsgs.SelectedText = "";
+            this.txtMsgs.SelectionLength = 0;
+            this.txtMsgs.SelectionStart = 0;
+            this.txtMsgs.ShortcutsEnabled = true;
+            this.txtMsgs.Size = new System.Drawing.Size(516, 296);
+            this.txtMsgs.TabIndex = 8;
+            this.txtMsgs.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.txtMsgs.UseSelectable = true;
+            this.txtMsgs.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtMsgs.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.lstClients);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(23, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 352);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel2.Controls.Add(this.txtUserName);
+            this.panel2.Controls.Add(this.btnLogin);
+            this.panel2.Controls.Add(this.metroLabel1);
+            this.panel2.Location = new System.Drawing.Point(3, 299);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(230, 48);
+            this.panel2.TabIndex = 10;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            // 
+            // 
+            // 
+            this.txtUserName.CustomButton.Image = null;
+            this.txtUserName.CustomButton.Location = new System.Drawing.Point(108, 1);
+            this.txtUserName.CustomButton.Name = "";
+            this.txtUserName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtUserName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtUserName.CustomButton.TabIndex = 1;
+            this.txtUserName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtUserName.CustomButton.UseSelectable = true;
+            this.txtUserName.CustomButton.Visible = false;
+            this.txtUserName.Lines = new string[0];
+            this.txtUserName.Location = new System.Drawing.Point(3, 20);
+            this.txtUserName.MaxLength = 32767;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.PasswordChar = '\0';
+            this.txtUserName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtUserName.SelectedText = "";
+            this.txtUserName.SelectionLength = 0;
+            this.txtUserName.SelectionStart = 0;
+            this.txtUserName.ShortcutsEnabled = true;
+            this.txtUserName.Size = new System.Drawing.Size(130, 23);
+            this.txtUserName.TabIndex = 9;
+            this.txtUserName.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.txtUserName.UseSelectable = true;
+            this.txtUserName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtUserName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(139, 20);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(88, 23);
+            this.btnLogin.Style = MetroFramework.MetroColorStyle.Black;
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.btnLogin.UseSelectable = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.btnSend);
+            this.panel3.Controls.Add(this.txtSend);
+            this.panel3.Location = new System.Drawing.Point(265, 362);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(516, 53);
+            this.panel3.TabIndex = 10;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(438, 3);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 47);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.btnSend.UseSelectable = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.txtSend.CustomButton.Image = null;
+            this.txtSend.CustomButton.Location = new System.Drawing.Point(383, 1);
+            this.txtSend.CustomButton.Name = "";
+            this.txtSend.CustomButton.Size = new System.Drawing.Size(45, 45);
+            this.txtSend.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtSend.CustomButton.TabIndex = 1;
+            this.txtSend.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtSend.CustomButton.UseSelectable = true;
+            this.txtSend.CustomButton.Visible = false;
+            this.txtSend.Lines = new string[0];
+            this.txtSend.Location = new System.Drawing.Point(3, 3);
+            this.txtSend.MaxLength = 32767;
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.PasswordChar = '\0';
+            this.txtSend.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtSend.SelectedText = "";
+            this.txtSend.SelectionLength = 0;
+            this.txtSend.SelectionStart = 0;
+            this.txtSend.ShortcutsEnabled = true;
+            this.txtSend.Size = new System.Drawing.Size(429, 47);
+            this.txtSend.TabIndex = 3;
+            this.txtSend.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.txtSend.UseSelectable = true;
+            this.txtSend.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtSend.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // frmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 438);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtMsgs);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmClient";
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.Text = "Storx";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.frmClient_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstClients;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox txtMsgs;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private MetroFramework.Controls.MetroButton btnLogin;
+        private MetroFramework.Controls.MetroTextBox txtUserName;
+        private MetroFramework.Controls.MetroButton btnSend;
+        private MetroFramework.Controls.MetroTextBox txtSend;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # StorxClient
+
+The Source Code of Storx Client.
+
+You can use this to make your own Storx Client with custom textures and IP connections!
+
+Also, you will need the Storx Server, you can find it on the main directory.
